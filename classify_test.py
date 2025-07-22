fc6e75ebd307025e460a42e18237b390bdd61e2d
Henrique Galvão <henriquegalvao@discente.ufg.br>
2025-07-22 00:25:28 -0300

    Update classify_test.py
    The test isn't quite finished since the model is still on training, although the system of "phishing points" is already implemented

--- classify_test.py
+++ classify_test.py
@@ -1,26 +1,53 @@
-from transformers import BertTokenizerFast, BertForSequenceClassification
-import torch
-from detect_words import encontrar_palavras_suspeitas
-from detect_character import analisar_mensagem
+from detect_words.py import retorno_Palavra(assunto_alvo,1)
+from detect_character.py import retorno_CaractereAssunto(assunto_alvo)
 
-# Carregar modelo
-tokenizer = BertTokenizerFast.from_pretrained("model/")
-model = BertForSequenceClassification.from_pretrained("model/")
-model.eval()
+def Calcular_Pontuacao_ConteudoAssunto(assunto_alvo,conteudo_alvo):
+  
+  # Peso e Notas Assunto
+  peso_Pa = 4.8
+  peso_Ca = 3.1
+  peso_Ga = 2.1
 
-def classificar_email(texto):
-    inputs = tokenizer(texto, return_tensors="pt", truncation=True, padding="max_length", max_length=256)
-    with torch.no_grad():
-        outputs = model(**inputs)
-    probs = torch.nn.functional.softmax(outputs.logits, dim=1)
-    classe = torch.argmax(probs).item()
-    return 1.8 if classe == 1 else 0
+  nota_Pa = retorno_Palavra(assunto_alvo,1)          # PONTUAÇÃO RELATIVA PALAVRAS-CHAVE
+  nota_Ca = retorno_CaractereAssunto(assunto_alvo)   # PONTUAÇÃO RELATIVA CARACTERES
+  nota_Ga = 10                                       # PONTUAÇÃO RELATIVA GRAMATICA
 
-email_teste=""
+  # Peso e Notas Conteudo
+  peso_Pc = 3.0
+  peso_Cc = 2.4
+  peso_Lc = 3.1
+  peso_Gc = 1.5
 
-# Teste
-print(
-    classificar_email(email_teste),
-    encontrar_palavras_suspeitas(email_teste),
-    analisar_mensagem(email_teste)
-    )
+  nota_Pc = retorno_Palavra(conteudo_alvo,0)           # PONTUAÇÃO RELATIVA PALAVRAS
+  nota_Cc = retorno_CaractereConteudo(conteudo_alvo)   # PONTUAÇÃO RELATIVA CARACTERES
+  nota_Lc = 10                                         # PONTUAÇÃO RELATIVA LLM
+  nota_Gc = 10                                         # PONTUAÇÃO RELATIVA GRAMATICA
+
+  # Dicionarios da Relação Peso x Nota
+  Assunto_PesoNota={peso_Pa:nota_Pa, peso_Ca:nota_Ca, peso_Ga:nota_Ga}
+  Conteudo_PesoNota={peso_Pc:nota_Pc, peso_Cc:nota_Cc, peso_Lc:nota_Lc, peso_Gc:nota_Gc}
+
+  # Cálculo da Nota
+  nota_AssTotal_numerador=0
+  nota_ConTotal_numerador=0
+
+  nota_AssTotal_denominador=0
+  nota_ConTotal_denominador=0
+
+  for peso,nota in Assunto_PesoNota.items():
+    nota_AssTotal_numerador+=nota*peso
+    nota_AssTotal_denominador+=peso
+    print(f'numerador: {nota_AssTotal_numerador}, denominador: {nota_AssTotal_denominador}')
+  print(nota_AssTotal_numerador/nota_AssTotal_denominador,end='\n')
+
+  for peso,nota in Conteudo_PesoNota.items():
+    nota_ConTotal_numerador+=nota*peso
+    nota_ConTotal_denominador+=peso
+    print(f'numerador: {nota_ConTotal_numerador}, denominador: {nota_ConTotal_denominador}')
+  print(nota_ConTotal_numerador/nota_ConTotal_denominador,end='\n\n')
+
+  pontuacao_total = ((nota_AssTotal_numerador/nota_AssTotal_denominador)+(nota_ConTotal_numerador/nota_ConTotal_denominador))*(0.3)
+
+  return round(pontuacao_total,3)
+
+print(Calcular_Pontuacao_ConteudoAssunto(assunto_especifico,email_especifico))
